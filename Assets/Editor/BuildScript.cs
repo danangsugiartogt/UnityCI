833374aa26cb468ec7dba1ee5465e270338257f9
danangmaleo <danang.sugiarto@maleo.co>
2025-07-04 16:57:27 +0700

    Add build script for android platform

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -17,9 +18,40 @@ public static class BuildScript
         // Build options
         BuildPlayerOptions options = new BuildPlayerOptions
         {
-            scenes = new[] { "Assets/Scenes/SampleScene.unity" },
+            scenes = EditorBuildSettings.scenes.Where(s => s.enabled).Select(s => s.path).ToArray(),
             locationPathName = outputPath,
             target = BuildTarget.WebGL,
+            options = BuildOptions.None
+        };
+
+        BuildPipeline.BuildPlayer(options);
+    }
+
+    [MenuItem("CI/Build Android CI")]
+    public static void BuildAndroidForCI()
+    {
+        // Setup keystore for signing
+        PlayerSettings.Android.useCustomKeystore = true;
+        PlayerSettings.Android.keystoreName = "keystore/user.keystore"; // rel. path dari project root
+        PlayerSettings.Android.keystorePass = System.Environment.GetEnvironmentVariable("KEYSTORE_PASS");
+        PlayerSettings.Android.keyaliasName = System.Environment.GetEnvironmentVariable("KEY_ALIAS");
+        PlayerSettings.Android.keyaliasPass = System.Environment.GetEnvironmentVariable("KEY_ALIAS_PASS");
+
+        // Android min and target version
+        PlayerSettings.Android.minSdkVersion = AndroidSdkVersions.AndroidApiLevel22;
+        PlayerSettings.Android.targetSdkVersion = AndroidSdkVersions.AndroidApiLevel33;
+
+        // Scripting backend
+        PlayerSettings.SetScriptingBackend(BuildTargetGroup.Android, ScriptingImplementation.IL2CPP);
+
+        // Architectures target
+        PlayerSettings.Android.targetArchitectures = AndroidArchitecture.ARMv7 | AndroidArchitecture.ARM64;
+
+       BuildPlayerOptions options = new BuildPlayerOptions
+        {
+            scenes = EditorBuildSettings.scenes.Where(s => s.enabled).Select(s => s.path).ToArray(),
+            locationPathName = "build/Android/Game.apk",
+            target = BuildTarget.Android,
             options = BuildOptions.None
         };
 
