bf9d481a526cd9253d56d513453cd3442c402c05
danangmaleo <danang.sugiarto@maleo.co>
2025-07-06 01:55:59 +0700

    Add more logs

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
@@ -35,10 +36,14 @@ public static class BuildScript
 
         // Setup keystore for signing
         PlayerSettings.Android.useCustomKeystore = true;
-        PlayerSettings.Android.keystoreName = GetArg("-androidKeystoreName");
-        PlayerSettings.Android.keystorePass = GetArg("-androidKeystorePass");
-        PlayerSettings.Android.keyaliasName = GetArg("-androidKeyaliasName");
-        PlayerSettings.Android.keyaliasPass = GetArg("-androidKeyaliasPass");
+        PlayerSettings.Android.keystoreName = GetArg("-androidKeystoreName")?.Trim();
+        PlayerSettings.Android.keystorePass = GetArg("-androidKeystorePass")?.Trim();
+        PlayerSettings.Android.keyaliasName = GetArg("-androidKeyaliasName")?.Trim();
+        PlayerSettings.Android.keyaliasPass = GetArg("-androidKeyaliasPass")?.Trim();
+
+        Debug.Log("keystore pass length: " + PlayerSettings.Android.keystorePass.Length);
+        Debug.Log("keystore alias length: " + PlayerSettings.Android.keyaliasName.Length);
+        Debug.Log("keystore alias pass length: " + PlayerSettings.Android.keyaliasPass.Length);
 
         // Android min and target version
         PlayerSettings.Android.minSdkVersion = AndroidSdkVersions.AndroidApiLevel22;
@@ -50,7 +55,7 @@ public static class BuildScript
         // Architectures target
         PlayerSettings.Android.targetArchitectures = AndroidArchitecture.ARMv7 | AndroidArchitecture.ARM64;
 
-       BuildPlayerOptions options = new BuildPlayerOptions
+        BuildPlayerOptions options = new BuildPlayerOptions
         {
             scenes = EditorBuildSettings.scenes.Where(s => s.enabled).Select(s => s.path).ToArray(),
             locationPathName = "build/Android/Game.apk",
@@ -67,7 +72,11 @@ public static class BuildScript
         for (int i = 0; i < args.Length - 1; i++)
         {
             if (args[i] == name)
+            {
+                Debug.Log("GetArg Found: " + args[i]);
+                Debug.Log("GetArg Returned: " + args[i + 1]);
                 return args[i + 1];
+            }
         }
 
         return null;
