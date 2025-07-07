1b16181b852090358e11220b09c3fecf904b2b70
danangmaleo <danang.sugiarto@maleo.co>
2025-07-07 09:50:47 +0700

    Enable webgl build

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -30,19 +30,12 @@ public static class BuildScript
     [MenuItem("CI/Build Android CI")]
     public static void BuildAndroidForCI()
     {
-        foreach (var arg in System.Environment.GetCommandLineArgs())
-            Debug.Log("CLI ARG: " + arg);
-
         // Setup keystore for signing
         PlayerSettings.Android.useCustomKeystore = true;
         PlayerSettings.Android.keystoreName = GetArg("-androidKeystoreName");
         PlayerSettings.Android.keystorePass = GetArg("-androidKeystorePass");
         PlayerSettings.Android.keyaliasName = GetArg("-androidKeyaliasName");
         PlayerSettings.Android.keyaliasPass = GetArg("-androidKeyaliasPass");
-
-        Debug.Log("keystore pass length: " + PlayerSettings.Android.keystorePass.Length);
-        Debug.Log("keystore alias length: " + PlayerSettings.Android.keyaliasName.Length);
-        Debug.Log("keystore alias pass length: " + PlayerSettings.Android.keyaliasPass.Length);
 
         // Android min and target version
         PlayerSettings.Android.minSdkVersion = AndroidSdkVersions.AndroidApiLevel22;
